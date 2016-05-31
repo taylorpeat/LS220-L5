bc22d193068fa7055233d21b30cec8a2d7bcd83f
Taylor Peat <taylorpeat@hotmail.com>
2016-05-30 22:55:03 -0400

    refactor js

--- guess_word/guess-a-word.js
+++ guess_word/guess-a-word.js
@@ -1,49 +1,74 @@
-var words = ["tidy", "pants", "slide", "dream"];
+var words = ["tidy", "pants", "slide", "hello"],
+    round;
 
-Game.prototype = {
+Round.prototype = {
   winningMessage: "Congratulations! You've guessed the word!!",
   losingMessage: "Sorry, you are out of guesses!",
-  watchLetter: function(e) {
-    letter = String.fromCharCode(e.which);
-    if (this.validLetter(e.which)) { this.processGuess(letter) };
-    if (this.guesses === 6) {
-      this.endRound(this.losingMessage, "lose");
-    };
-    if (this.correct_letters.length === this.word.length) { 
-      this.endRound(this.winningMessage, "win");
-    };
+
+  processGuess: function(e) {
+    var letter = String.fromCharCode(e.which),
+        indicies;
+
+    if (this.invalidLetter(e.which)) { return }
+
+    indicies = this.findLetterIndicies(letter);
+    if (this.updateGuess(indicies, letter)) { this.checkIfRoundEnded() }
   },
 
-  processGuess: function(letter) {
+  invalidLetter: function(charCode) {
+    return charCode > 122 || charCode < 97;
+  },
+
+  findLetterIndicies: function(letter) {
     var regex = new RegExp(letter, "gi"),
         indicies = [];
-    
+
     while (result = regex.exec(this.word)) {
       indicies.push(result.index);
     }
-
-    if (indicies[0] || indicies[0] === 0) {
-      if (!this.correct_letters.includes(letter)) {
-      this.fillLetters(indicies, letter);
-      } 
-    } else {
-      this.addGuess(letter);
-      this.guesses ++;
-      $("#apples").removeClass().addClass("guess_" + this.guesses);
-    }
-    
-    return !!indicies[0];
+    return indicies;
   },
 
-  fillLetters: function(indicies, letter) {
+  updateGuess: function(indicies, letter) {
+    if (indicies[0] || indicies[0] === 0) {
+      if (this.correct_letters.includes(letter)) { return false }
+      this.updateRightGuess(indicies, letter);
+    } else {
+      this.updateWrongGuess(letter);
+    }
+    return true;
+  },
+
+  updateRightGuess: function(indicies, letter) {
     indicies.forEach(function(index) {
       $("#word span").eq(index).text(letter);
       this.correct_letters.push(letter);
     }, this);
   },
 
-  validLetter: function(charCode) {
-    return charCode < 123 && charCode > 96;
+  updateWrongGuess: function(letter) {
+    this.addGuess(letter);
+    this.guesses ++;
+    $("#apples").removeClass().addClass("guess_" + this.guesses);
+  },
+
+  addGuess: function(letter) {
+    $("#guesses").append("<span>" + letter + "</span>");
+  },
+
+  checkIfRoundEnded: function() {
+    if (this.guesses === 6) {
+      this.endRound(this.losingMessage, "lose");
+    } else if (this.correct_letters.length === this.word.length) { 
+      this.endRound(this.winningMessage, "win");
+    }
+  },
+
+  endRound: function(message, result) {
+    $("p").eq(0).text(message);
+    $("body").addClass(result);
+    $(document).unbind();
+    $("a").show();
   },
 
   createBlanks: function() {
@@ -56,57 +81,51 @@ Game.prototype = {
     $("#word").append("<span></span>");
   },
 
-  addGuess: function(letter) {
-    $("#guesses").append("<span>" + letter + "</span>");
-  },
-
-  endRound: function(message, result) {
-    $("p").eq(0).text(message);
-    $("body").addClass(result);
-    $(document).unbind();
-    $("a").show();
-  },
-
   gameOver: function() {
-    $("p").eq(0).text("There are no more words left to play.")
+    $("p").eq(0).text("There are no more words left to play.");
     $("body").addClass("game_over");
-    $("#apples").hide();
-    $("#tree").attr("src", "images/tree_of_dead_hopes.png");
+    $("#images").prepend("<img src='images/tree_of_dead_hopes.png' id='dead_tree'>");
+    $("#dead_tree").fadeIn(1000);
+    $("#apples").hide("explode", {pieces: 9}, 1000);
+    $("#tree").hide("explode", {pieces: 49}, 1000);
     $("a").hide();
     $(document).unbind();
   }
 }
 
-function Game() {
+function Round() {
   this.guesses = 0;
   this.correct_letters = [];
   this.word = getWord.call(this);
-  if (this.word === undefined) { this.gameOver(); };
+
+  if (this.word === undefined) { this.gameOver(); return }
+
   this.createBlanks();
 
-  $(document).on("keypress", this.watchLetter.bind(this));
+  $(document).on("keypress", this.processGuess.bind(this));
+}
+
+function getWord() {
+  if (words[0] === undefined) { return }
+  var idx = Math.floor(Math.random() * words.length);
+  var new_word = words.splice(idx, 1)[0];
+  return new_word;
 }
 
 $("a").on("click", function(e) {
   e.preventDefault();
 
   $("a").hide();
-  newRound();
+  startRound();
 });
 
-function getWord() {
-  var idx = Math.floor(Math.random() * words.length);
-  var new_word = words.splice(idx, 1)[0];
-  return new_word;
-}
-
-function newRound() {
+function startRound() {
   $(document).unbind();
   $("span").remove();
   $("body").removeClass();
   $("p").eq(0).text("");
   $("#apples").removeClass();
-  game = new Game();
+  round = new Round();
 }
 
-var game = new Game();
+round = new Round();
